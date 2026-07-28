e06cf73319cff848e5d02302ed38ad3a7c8a2617
busilakshmi657-gif <busilakshmi657@gmail.com>
2026-07-28 22:01:19 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,9 @@
+console.log("Portfolio Loaded Successfully");
+window.onload =function(){
+    alert("welcome to my portfolio!")
+}
+function sendMessage(){
+
+    alert("Thank you for contacting me!");
+
+}
